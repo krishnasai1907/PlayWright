be6e9f5d37faf6ec3956552a169b80d20d37f92e
krishnasai1907 <57449497+krishnasai1907@users.noreply.github.com>
2022-06-07 12:46:11 +0530

    new playwright

---
+++ src/main/java/com/first/FirstPlayWright.java
@@ -0,0 +1,43 @@
+package com.first;
+
+import com.microsoft.playwright.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class FirstPlayWright {
+    public static void main(String[] args) {
+
+        Playwright playwright=Playwright.create();
+        Browser browser=playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false).setArgs(Collections.singletonList("--start-maximized")));
+        BrowserContext context=browser.newContext();
+        Page page=context.newPage();
+        page.navigate("https://opensource-demo.orangehrmlive.com/");
+        System.out.println("opening");
+        page.locator("id=txtUsername").type("Admin");
+        page.locator("id=txtPassword").type("admin123");
+        page.locator("id=btnLogin").click();
+        page.pause();
+        // Click b:has-text("Leave")
+        page.locator("b:has-text(\"Leave\")").click();
+        // assertThat(page).hasURL("https://opensource-demo.orangehrmlive.com/index.php/leave/viewLeaveList");
+        // Check #leaveList_chkSearchFilter_checkboxgroup_allcheck
+        page.locator("#leaveList_chkSearchFilter_checkboxgroup_allcheck").check();
+        // Click input[name="leaveList\[txtEmployee\]\[empName\]"]
+        page.locator("input[name=\"leaveList\\[txtEmployee\\]\\[empName\\]\"]").click();
+        // Fill input[name="leaveList\[txtEmployee\]\[empName\]"]
+        page.locator("input[name=\"leaveList\\[txtEmployee\\]\\[empName\\]\"]").fill("Krishnasai");
+        // Press Enter
+        page.locator("input[name=\"leaveList\\[txtEmployee\\]\\[empName\\]\"]").press("Enter");
+        // Select 13
+        page.locator("select[name=\"leaveList\\[cmbSubunit\\]\"]").selectOption("13");
+        // Click text=Search
+        page.locator("text=Search").click();
+        // assertThat(page).hasURL("https://opensource-demo.orangehrmlive.com/index.php/leave/viewLeaveList");
+        // Close page
+        page.close();
+        browser.close();
+    }
+
+}
